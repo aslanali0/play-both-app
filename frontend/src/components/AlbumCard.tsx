20e5fcc86f9d0f49bab6b0d25c4dda2dcee9ad4d
Ali Aslan <aliaslan@MacBookPro.modem.turktelekom>
2026-03-12 01:41:28 +0300

    small change

--- frontend/src/components/AlbumCard.tsx
+++ frontend/src/components/AlbumCard.tsx
@@ -1,22 +1,35 @@
-import type {Album} from '../types/content.ts'
-import SongCard from './SongCard.tsx';
+import type { Album } from "../types/content.ts";
+import SongCard from "./SongCard.tsx";
 
-const AlbumCard = ({albumData, favList}: {albumData : Album, favList: any}) => {
-  
-
+const AlbumCard = ({
+  albumData,
+  favList,
+}: {
+  albumData: Album;
+  favList: any;
+}) => {
   return (
-    <div>{albumData &&       
-      <div>
-        <h1>{albumData.album_title}</h1>
-        <ul>
-          {albumData.map((song) => (
-          <SongCard key={song?.youtube_url || `https://www.youtube.com/results?search_query=${song?.title}`} favList={favList} songData{song}/>
-          ))}
-        </ul>  
-      </div>
-    }
+    <div>
+      {albumData && (
+        <div>
+          <h1>{albumData.album_title}</h1>
+          <ul>
+            {albumData.map((song) => (
+              <SongCard
+                key={
+                  song?.youtube_url ||
+                  `https://www.youtube.com/results?search_query=${song?.title}`
+                }
+                favList={favList}
+                songData
+                {...song}
+              />
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default AlbumCard;
