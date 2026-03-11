afce8667638deea7483f9e7da472f00aa92059e5
aslanali0 <alyadaslan@gmail.com>
2026-03-12 02:48:59 +0300

    api fix

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,30 +1,33 @@
-import axios, {type InternalAxiosRequestConfig}from 'axios';
+import axios, { type InternalAxiosRequestConfig } from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || '/api'
-
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+if (!API_BASE_URL) {
+  throw new Error(
+    "Missing VITE_API_URL. Set it in your environment variables.",
+  );
+}
 const api = axios.create({
-  baseURL:API_BASE_URL, 
+  baseURL: API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
-api.interceptors.request.use((config: InternalAxiosRequestConfig) =>{
-    const token = localStorage.getItem('token');
- if(token) {
-    config.params={
-    ...config.params,
-    token: token,
-  };
-  }
-  return config;
-  }, (error) => {
-  return Promise.reject(error);
-}
-
- 
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.params = {
+        ...config.params,
+        token: token,
+      };
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
 );
 /*All requests will be transfered in this file*/
-
 
 export default api;
