098e25cbfc4869de95be0e3e469b9cf7daee8043
aslanali0 <alyadaslan@gmail.com>
2026-03-12 03:31:48 +0300

    Feed Page error fix

--- frontend/src/pages/FeedPage.tsx
+++ frontend/src/pages/FeedPage.tsx
@@ -8,28 +8,35 @@ const API_URL = "/posts/all";
 
 const FeedPage = () => {
   const [posts, setPosts] = useState<Post[]>([]);
+
   useEffect(() => {
     const handlePosts = async () => {
       try {
         const response = await api.get(API_URL);
-        if (response) {
-          setPosts(response.data);
-        }
+
+        const data = Array.isArray(response.data)
+          ? (response.data as Post[])
+          : [];
+        setPosts(data);
       } catch (error) {
         console.log(error);
+        setPosts([]);
       }
     };
+
     handlePosts();
   }, []);
+
   return (
     <div className="min-h-screen w-full max-w-[100vw] overflow-x-hidden flex flex-col items-center pt-20 pb-60 px-4 md:px-8">
       <div className="w-full max-w-2xl lg:max-w-3xl flex flex-col gap-6">
-        {posts && posts.length > 0 ? (
+        {posts.length > 0 ? (
           posts.map((post) => <PostCard key={post.post_id} post={post} />)
         ) : (
           <div className="text-gray-400 text-center mt-10">No posts yet...</div>
         )}
       </div>
+
       <div className="fixed bottom-6 z-40 w-[90vw] max-w-2xl left-1/2 -translate-x-1/2">
         <PostForm />
       </div>
